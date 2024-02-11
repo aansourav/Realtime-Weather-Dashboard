c403ef3f3ee6a7ab2d75cf63d187ddf7d37e8b98
SoUrAv <aansourav@gmail.com>
2024-02-11 21:15:51 +0600

    API, Context & Provider added

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,13 +1,16 @@
+
 import Header from "./components/header/Header";
 import WeatherBoard from "./components/weatherBoard/WeatherBoard";
 
+
 const Page = () => {
+
     return (
-        <div className="grid place-items-center h-screen" >
+        <div className="grid place-items-center h-screen">
             <Header />
             <main>
                 <section>
-                    <WeatherBoard/>
+                    <WeatherBoard />
                 </section>
             </main>
         </div>
